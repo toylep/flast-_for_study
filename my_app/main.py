8049badc71d75cc98fafb33b567e355a8331050e
cnrprod-team-65719 <toylenium@mail.ru>
2024-03-03 07:51:32 +0800

    Add

--- my_app/main.py
+++ my_app/main.py
@@ -9,6 +9,14 @@ class Video:
     duration:int
     count_likes:int
 
+
+@dataclass
+class Card:
+    name:str
+    text:str
+    count_likes:int
+
+
 video_list = [
     Video(
         name="Black Sabbath - Paranoid",
@@ -17,9 +25,21 @@ video_list = [
     )
 ]
 
+cards = [
+    Card(
+        name="Паста про жаренный суп",
+        text="Мой батя делает вообще адовые блюда",
+        count_likes=10,
+    )
+]
+
 @server.get("/videos")
 def get_videos():
     return render_template("index.html",videos=video_list)
+
+@server.get("/posts")
+def get_posts():
+    return render_template("posts.html",posts=cards)
 
 @server.post("/videos/add")
 def add_video():
@@ -31,7 +51,6 @@ def add_video():
     video_list.append(video)
     print(video_list)
     return "Видео добавлено"
-
 
 if __name__=='__main__':
     server.run()
